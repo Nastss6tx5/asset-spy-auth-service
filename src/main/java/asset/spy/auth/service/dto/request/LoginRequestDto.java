3e42f5c48ca13feb6d08e0812a601d3b30a6dda6
Anastasiia <nastasiia227@gmail.com>
2025-03-27 11:44:56 +0300

    feat(STOCK-83) unused annotation deleted

--- src/main/java/asset/spy/auth/service/dto/request/LoginRequestDto.java
+++ src/main/java/asset/spy/auth/service/dto/request/LoginRequestDto.java
@@ -1,11 +1,9 @@
 package asset.spy.auth.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class LoginRequestDto {
     @NotBlank(message = "Login cannot be blank")
     private String login;
--- src/main/java/asset/spy/auth/service/dto/response/UserInfoResponseDto.java
+++ src/main/java/asset/spy/auth/service/dto/response/UserInfoResponseDto.java
@@ -1,14 +1,12 @@
 package asset.spy.auth.service.dto.response;
 
 import asset.spy.auth.service.model.Role;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
 @Data
-@Builder
 public class UserInfoResponseDto {
     private String username;
     private String login;
